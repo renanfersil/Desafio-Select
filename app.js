eb3ce28175f951d2c1d5cf8ab3ff4c19ef612443
Renan Fernandes <101608000+renanfersil@users.noreply.github.com>
2023-10-10 11:49:12 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+lucide.createIcons();
+
+/*
+const optionview = document.getElementById("options-view-button")
+Selection.addEventListener("change", function() {
+    const selectedValue = select.value
+
+    // esconder as opções
+    const options = select.querySelectorAll("option")
+    options.forEach(option => {
+        option.style.display = "none"
+    })
+
+    // exibe as opções
+    const selectedOption = select.querySelector(`option[VALUE="${selectedValue}"]`)
+    if (selectedOption) {
+        selectedOption.style.display = "block"
+    }
+})
+*/
